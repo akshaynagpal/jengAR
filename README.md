aa54dbe6a06eedb9ec304de2e2cd75fcd25b483b
Akshay Nagpal <akshay2626@gmail.com>
2017-04-12 02:09:34 -0400

    sample tower

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
-# 3dui_final_project
-3dui_final_project
+# jengAR
+jenga block asset link - https://www.assetstore.unity3d.com/en/#!/content/13213
---
+++ jengAR/Assets/Scripts/rotateCamera.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class rotateCamera : MonoBehaviour {
+
+    public GameObject tower;
+
+    private void Start()
+    {
+    }
+    void  Update()
+    {
+        transform.LookAt(tower.gameObject.transform);
+        transform.Translate(Vector3.right * Time.deltaTime);
+    }
+}
