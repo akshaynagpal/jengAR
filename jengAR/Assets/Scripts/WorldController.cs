1309ddfdc87589497a8419f6eb192a4bfeb6eb0c
Tiancheng Zhang <ztc@dyn-209-2-210-91.dyn.columbia.edu>
2017-04-18 13:30:40 -0400

    Added basic hover ring functionality.

---
+++ jengAR/Assets/Scripts/WorldController.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WorldController : MonoBehaviour {
+
+	Vector3 nextPos;
+	public Vector3 moveDelta;
+
+	private int moveType;
+
+	// Use this for initialization
+	void Start () {
+		nextPos = transform.localPosition;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.localPosition += moveType * moveDelta;
+	}
+
+	public void BeginUp() {
+		moveType = 1;
+	}
+
+	public void BeginDown() {
+		moveType = -1;
+	}
+
+	public void EndMove() {
+		moveType = 0;
+	}
+}
