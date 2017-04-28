cb3fbbf1066037c257c2310b6614528825005047
Tiancheng Zhang <ztc@dyn-209-2-210-102.dyn.columbia.edu>
2017-04-27 21:59:53 -0400

    Basic floor indicator now available.

---
+++ jengAR/Assets/Scripts/PlatformController.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlatformController : MonoBehaviour {
+
+	public GameObject block;
+	private float blockHeight;
+
+	public GameObject ground;
+
+	private int floor;
+
+	GameObject[] indicators;
+
+	// Use this for initialization
+	void Start () {
+		blockHeight = block.GetComponent<Renderer> ().bounds.size.y;
+		indicators = GameObject.FindGameObjectsWithTag ("LvlIndicator");
+		floor = 0;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		floor = (int)((transform.localPosition.y - ground.transform.localPosition.y) / blockHeight);
+		foreach (GameObject indicator in indicators) {
+			indicator.GetComponent<TextMesh> ().text = floor.ToString ();
+		}
+	}
+}
